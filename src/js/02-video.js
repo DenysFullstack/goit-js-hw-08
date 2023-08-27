c3cb99fbb39726ca7e8614d6c50b97ff5fd419d8
DenysFullstack <allcarinae@gmail.com>
2023-08-27 23:23:17 +0300

    third commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+player.on('timeupdate', throttle(OnPlayer, 1000))
+const STORAGE_KEY = localStorage.getItem('videoplayer-current-time');
+
+function OnPlayer(data){
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(Number(STORAGE_KEY));
+
+// let timeStoped = localStorage.getItem('videoplayer-current-time');
+// if (timeStoped === null) {
+//   timeStoped = 0;
+// }
+
+
+// let timeStoped = localStorage.getItem('videoplayer-current-time');
+// if (timeStoped === null) {
+//   timeStoped = 0;
+// }
+// const onPlay = e => {
+//   localStorage.setItem('videoplayer-current-time', `${e.seconds}`);
+// };
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+
+console.dir(player)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input');
+const inputMeassage = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInfo, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const dataInfo = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+if (dataInfo) {
+  const { email, message } = dataInfo;
+  inputEmail.value = email;
+  inputMeassage.value = message;
+} else {
+  inputEmail.value = '';
+  inputMeassage.value = '';
+}
+
+function onFormInfo(e) {
+  const { email, message } = e.currentTarget.elements;
+  if (email.value === '' || message.value === '') return;
+  const info = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(info));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.clear();
+}
